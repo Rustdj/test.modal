27efde1dda22ec6f3cfeac26060f8d53852e30bd
Rustdj <rustdj@icloud.com>
2021-08-10 23:54:32 +0300

    modal commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// Modal
+
+const modalTrigger = document.querySelector('[data-modal]'),
+      modal = document.querySelector('.valut__modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.addEventListener('click', () => {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+
+});
+modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+});
